30f3500e0d0dbad3f43c221f4743ec17b3dc6039
ADITYA SINGH <adityasingh80026@gmail.com>
2026-05-14 21:10:44 +0530

    fixed loading of site

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { store, persistor } from './store/index';
 import App from './App';
 import './index.css';
 
+setupListeners(store.dispatch);
+
+const PageLoader = () => (
+  <div className="min-h-screen flex items-center justify-center bg-background">
+    <div className="w-8 h-8 border-2 border-white border-t-transparent rounded-full animate-spin" />
+  </div>
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<PageLoader />} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
